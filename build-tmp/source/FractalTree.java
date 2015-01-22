094081b1ff5a134214553f2418c463a65a20eb10
unknown <thwhite@697-334-Linhir.sf.k12.internal>
2015-01-22 10:33:46 -0800

    Location adjustment

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -26,11 +26,11 @@ public void draw()
 {   
 	background(0);   
 	stroke(0, 0, 255);   
-	line(320,480,320,580);   
-	drawBranches(320, 580, 100, Math.PI/2, .8f);  //will add later 
+	line(320,520,320,620);   
+	drawBranches(320, 620, 100, Math.PI/2, .8f);  //will add later 
 	stroke(0, 255, 0);
-	line(320, 480, 320, 380);
-	drawBranches(320, 380, 100, 3*Math.PI/2, .4f);
+	line(320, 520, 320, 420);
+	drawBranches(320, 420, 100, 3*Math.PI/2, .4f);
 } 
 public void drawBranches(int x,int y, double branchLength, double angle, double bAngle) 
 {   
