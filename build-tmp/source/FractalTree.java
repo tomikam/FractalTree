6dd982ef0fb54f3ad60431ca4853fefab338409b
unknown <thwhite@697-334-Linhir.sf.k12.internal>
2015-01-22 10:26:02 -0800

    First workign version plus cool tree

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 10; 
-private double branchAngle = .8;  
+private double branchAngle = .8f;  
 public void setup() 
 {   
 	size(640,960);    
@@ -32,3 +48,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 	}
 	  
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
